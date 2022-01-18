1a8d88e957ec05a53e9771130a7ba68e6a0182af
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-01-18 12:19:29 +0000

    start on js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let startButton = document.getElementsByClassName('start-btn')
+document.addEventListener('click', startGame)
+
+
+
+function startGame () {
+    console.log('game started');
+    startButton.classList.add("hide");
+} 
+
+function findQuestion () {}
+
+function selectAnswer () {}
+
+function checkAnswer () {}
