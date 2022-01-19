514a60449f459932c40fe35bd51fa69dfea0ec6a
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-01-19 12:15:18 +0000

    add questions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
-let startButton = document.getElementsByClassName('start-btn')
-document.addEventListener('click', startGame)
 
-
+let randomQuestions, currentQuestion;
+let questionContainer = document.getElementsByClassName('question-container');
+let questionElement = document.getElementsByClassName('question');
+let answerBtns = document.getElementsByClassName('answer-btns');
 
 function startGame () {
-    console.log('game started');
-    startButton.classList.add("hide");
+    console.log('game started');   
 } 
 
-function findQuestion () {}
+function findQuestion () {
+    showQuestion();
+}
+
+function showQuestion(){
+    
 
 function selectAnswer () {}
 
 function checkAnswer () {}
+
+let questions = [ 
+    {
+        question:'What is the platform number for the train to hogwarts?',
+        answers:[
+            {text: '10 1/2', correct: false},
+            {text: '9 3/4', correct: true},
+            {text:'7 1/2', correct: false},
+            {text: '8 3/4', correct:false}  
+        ]
+    }
+]
